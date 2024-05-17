74a72a80b27a00e29d12f8ef3252da688db97078
danlebedev <lebedev.py@gmail.com>
2024-05-17 14:20:03 +0300

    add class Folder in telegram_logic.py

--- telegram_logic.py
+++ telegram_logic.py
@@ -12,3 +12,22 @@ class Telegram():
     def is_started(self) -> bool:
         if self.session:
             return self.session.running()
+
+
+class Folder():
+    def __init__(self, index, session):
+        self.parent_classname = 'androidx.recyclerview.widget.RecyclerView'
+        self.classname = 'android.view.View'
+        self.index = index
+        self.session = session
+
+    def connect(self):
+        try:
+            self.session(className=self.parent_classname) \
+                .child(className=self.classname, index=self.index) \
+                .click()
+        finally:
+            return self.is_connected()
+
+    def is_connected(self):
+        return self.session(className=self.classname, index=self.index, selected=True).exists

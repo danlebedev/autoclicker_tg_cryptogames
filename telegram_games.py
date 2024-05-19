ac0c2c5aa40a45f3ff45e7e1b4ce10dd6292f620
danlebedev <lebedev.py@gmail.com>
2024-05-19 12:17:12 +0300

    add class Blum()

--- telegram_games.py
+++ telegram_games.py
@@ -18,3 +18,25 @@ class HarvestMoon():
             self.bot.session.click(*self.button)
             sleep(10)
             self.bot.run()
+
+
+class Blum():
+    name = 'Blum'
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.button = (0.500, 0.870)
+
+    def play(self):
+        texts = ('Launch Blum', 'Claim')    # todo: add text choice.
+        try:
+            self.tg.session(className='android.widget.Button', text=texts[0]).click()
+        except:
+            pass
+        else:
+            sleep(10)
+            self.tg.session.click(*self.button)
+            sleep(5)
+            self.tg.session.click(*self.button)
+            sleep(5)
+            self.tg.session.press('back')

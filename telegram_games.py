16f7b6423e7ebc75363a575238751185397ad779
danlebedev <lebedev.py@gmail.com>
2024-05-19 12:24:53 +0300

    add class HamsterKombat & add func clicker() & replace tg obj to bot obj in class Blum

--- telegram_games.py
+++ telegram_games.py
@@ -1,4 +1,12 @@
 from time import sleep
+from threading import Thread
+from random import randint
+
+
+def clicker(d, x, y, count):
+    y += randint(-50, +50) / 1000
+    for _ in range(count):
+        d.click(x, y)
 
 
 class HarvestMoon():
@@ -30,13 +38,47 @@ class Blum():
     def play(self):
         texts = ('Launch Blum', 'Claim')    # todo: add text choice.
         try:
-            self.tg.session(className='android.widget.Button', text=texts[0]).click()
+            self.bot.session(className='android.widget.Button', text=texts[0]).click()
         except:
             pass
         else:
             sleep(10)
-            self.tg.session.click(*self.button)
+            self.bot.session.click(*self.button)
             sleep(5)
-            self.tg.session.click(*self.button)
+            self.bot.session.click(*self.button)
             sleep(5)
-            self.tg.session.press('back')
+            self.bot.session.press('back')
+
+
+class HamsterKombat():
+    name = 'Hamster Kombat'
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.thanks = (0.500, 0.830)
+        self.hamster = (0.200, 0.630)
+
+    def play(self):
+        try:
+            self.bot.bot_run()
+        except:
+            pass
+        else:
+            sleep(10)
+            self.bot.session.click(*self.thanks)
+            self.multi_threads(10)
+            self.bot.bot_run()
+
+    def multi_threads(self, thread_count=0):
+        threads = []
+        for _ in range(thread_count):
+            threads.append(Thread(
+                target=clicker,
+                args=(self.bot.session, *self.hamster, 80)
+            ))
+            self.hamster[0] += 0.6 / thread_count
+        for thread in threads:
+            sleep(0.17)
+            thread.start()
+        for thread in threads:
+            thread.join()

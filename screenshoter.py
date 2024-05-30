5fb352caa12c91179e8bb854e0be452eabedf96e
danlebedev <lebedev.py@gmail.com>
2024-05-30 12:17:10 +0300

    refactoring multi_screenshots() with Emulator methods

--- screenshoter.py
+++ screenshoter.py
@@ -1,9 +1,6 @@
 from logic.system_logic import ADB, Emulator
 
 
-count = 0
-
-
 def menu(emulator):
     while True:
         key = input(
@@ -25,17 +22,12 @@ def menu(emulator):
 
 def multi_screenshots(emulator):
     input('Press enter to start screenshots record: ')
-    global count
     screenshots = []
-    for _ in range(100):
-        image = emulator.device.screenshot()
-        screenshots.append(image)
+    for _ in range(10):
+        screenshots.append(emulator.make_screenshot())
 
-    for index in range(len(screenshots)):
-        image = screenshots[index]
-        image.save(f'screenshots/{index + count}.png', format='PNG')
-    
-    count += len(screenshots)
+    for screenshot in screenshots:
+        emulator.save_screenshot(*screenshot)
 
 
 def main():

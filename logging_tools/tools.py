c192f6c210814585832f254d9c9d297a31c8a363
danlebedev <lebedev.py@gmail.com>
2024-06-04 11:48:54 +0300

    add read_state()

--- logging_tools/tools.py
+++ logging_tools/tools.py
@@ -10,5 +10,17 @@ def screenshot(emulator):
 
 
 def write_state(state: dict):
+    # TODO: add checking folder exists.
+    # TODO: rewrite path generation in open() with os lib.
     with open(f'{SESSION}/state.json', 'w') as f:
         json.dump(state, f)
+
+
+def read_state():
+    # TODO: add checking folder exists.
+    # TODO: rewrite path generation in open() with os lib.
+    try:
+        with open(f'{SESSION}/state.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return None

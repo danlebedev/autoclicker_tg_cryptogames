907dbaec4db93c553ce395dc38e785dc019f29cd
danlebedev <lebedev.py@gmail.com>
2024-06-04 11:40:54 +0300

    add write_state()

--- logging_tools/tools.py
+++ logging_tools/tools.py
@@ -1,6 +1,14 @@
+import json
+
 SCREENSHOTS = 'last_session/screenshots'
+SESSION = 'last_session/session'
 
 
 def screenshot(emulator):
     # TODO: add checking folder exists.
     emulator.make_and_save_screenshot(folder=SCREENSHOTS)
+
+
+def write_state(state: dict):
+    with open(f'{SESSION}/state.json', 'w') as f:
+        json.dump(state, f)

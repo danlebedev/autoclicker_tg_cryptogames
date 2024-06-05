a207bbd7aaa3cc21931c91a0360306f85a9f2252
danlebedev <lebedev.py@gmail.com>
2024-06-05 22:14:49 +0300

    create computer_vision.py and add search_objects_by_color()

---
+++ logic/computer_vision.py
@@ -0,0 +1,58 @@
+from cv2 import medianBlur, cvtColor, COLOR_BGR2HSV, \
+    inRange, findContours, RETR_LIST, CHAIN_APPROX_SIMPLE, \
+    fitEllipse
+from numpy import array, uint8
+
+
+def search_objects_by_color(
+        image,
+        color_min,
+        color_max,
+    ):
+    # Делаем размытие изображения.
+    image_blured = medianBlur(
+        src=image,
+        ksize=19,
+    )
+    # Конвертируем исходное изображение в цветовую модель HSV.
+    hsv_img = cvtColor(
+        src=image_blured,
+        code=COLOR_BGR2HSV,
+    )
+    # Подмираем минимальные и максимальные параметры цветового фильтра
+    # для выделения объектов.
+    hsv_min = array(
+        object=color_min,
+        dtype=uint8,
+    )
+    hsv_max = array(
+        object=color_max,
+        dtype=uint8,
+    )
+    # Применяем цветовой фильтр к исходному изображению.
+    hsv_mask = inRange(
+        src=hsv_img,
+        lowerb=hsv_min,
+        upperb=hsv_max,
+    )
+
+    # Ищем контуры и записываем их в переменную.
+    contours, hierarchy = findContours(
+        image=hsv_mask,
+        mode=RETR_LIST,
+        method=CHAIN_APPROX_SIMPLE,
+    )
+
+    # Будем хранить найденные координаты:
+    coordinates = []
+    # Перебираем первые 5 контуров в цикле.
+    for contour in contours[:5]:
+        # Выбираем контуры с длиной больше 40 точек.
+        if len(contour) > 40:
+            # Записываем контур в форме эллипса.
+            # fitEllipse возвращает кортеж:
+            # ((x=координата центра, y=координата центра), (a=большая полуось, b=малая полуось), t=угол врщения)
+            ellipse = fitEllipse(contour)
+            # Получаем координаты центра эллипса, приводя их к целому числу.
+            coordinates.append(map(int, ellipse[0]))
+    return coordinates

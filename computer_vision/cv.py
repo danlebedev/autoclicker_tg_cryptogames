0c8827e8b15c3f4f3c89dd17cf3e93023339a5de
danlebedev <lebedev.py@gmail.com>
2024-06-11 18:02:30 +0300

    add search_pixel_in_array() and rename search_pixel() to search_pixel_in_list()

--- computer_vision/cv.py
+++ computer_vision/cv.py
@@ -1,10 +1,13 @@
 from cv2 import findContours, RETR_LIST, CHAIN_APPROX_SIMPLE, \
     fitEllipse
-from .tools import negative_index
+from .tools import negative_index, array_index
+from numpy.typing import NDArray
 
 
 def search_contour_coordinates(hsv_mask):
     """
+    Средняя скорость. Скорость не сильно уменьшается при увеличении разрешения.
+
     return: [((x=координата центра, y=координата центра), (a=большая полуось, b=малая полуось), t=угол врщения), ...]
     """
     # Ищем контуры и записываем их в переменную.
@@ -29,7 +32,7 @@ def search_contour_coordinates(hsv_mask):
     return coordinates
 
 
-def search_pixel(
+def search_pixel_in_list(
         image: list,
         color_min: tuple,
         color_max: tuple,
@@ -39,6 +42,11 @@ def search_pixel(
         reverse=False,
     ):
     """
+    Низкая скорость. Скорость почти не увеличивается при увеличении значений step_.
+    При значениях step_ = 1 имеет наивысшую скорость из всех остальных search_pixel_() функций,
+    но медленнее search_contour_coordinates().
+    При значениях step_ > 20 медленее search_contour_coordinates().
+
     image: converted to list image;
     color_min: (r, g, b);
     color_max: (r, g, b);
@@ -71,6 +79,60 @@ def search_pixel(
                     (pixel [2] >= color_min[2] and pixel[2] <= color_max[2]):
                     pixel_index = string_slice.index(pixel) * abs(step_pixel)
                     string_index = image_slice.index(string) * abs(step_string)
+                    if reverse:
+                        return abs(negative_index(string, pixel_index) + 1), abs(negative_index(image, string_index + 1))
+                    return pixel_index, string_index
+    else:
+        return None
+
+
+def search_pixel_in_array(
+        image: NDArray,
+        color_min: tuple,
+        color_max: tuple,
+        skip_pixels: tuple = None,
+        step_string=1,
+        step_pixel=1,
+        reverse=False,
+    ):
+    """
+    Средняя скорость. Скорость значительно увеличивается при увеличении значений step_.
+    При значениях step_ = 1 имеет самую низкую скорость из всех остальных search_pixel_() функций и
+    медленнее search_contour_coordinates().
+    При значениях step_ > 20 быстрее search_contour_coordinates().
+
+    image: converted to list image;
+    color_min: (r, g, b);
+    color_max: (r, g, b);
+    skip_pixels: ((r, g, b), (r, g, b) ...)
+    step_string: step of selected elements from the array obtained from the image by y axis;
+    step_pixel: step of selected elements from the array obtained from the image by x axis;
+    reverse: True - search from bottom right to top left;
+    return: (x, y) coordinates.
+    """
+    if reverse:
+        step_string = -step_string
+        step_pixel = -step_pixel
+
+    image_slice = image[::step_string]
+    for string in image_slice:
+        string_slice = string[::step_pixel]
+        for pixel in string_slice:
+            skip = False
+            if skip_pixels:
+                for skip_pixel in skip_pixels:
+                    if (pixel[0] == skip_pixel[0]) and \
+                        (pixel[1] == skip_pixel[1]) and \
+                        (pixel[2] == skip_pixel[2]):
+                        skip = True
+                        break
+
+            if not skip:
+                if (pixel[0] >= color_min[0] and pixel[0] <= color_max[0]) and \
+                    (pixel[1] >= color_min[1] and pixel[1] <= color_max[1]) and \
+                    (pixel [2] >= color_min[2] and pixel[2] <= color_max[2]):
+                    pixel_index = array_index(string_slice, pixel, 1) * abs(step_pixel)
+                    string_index = array_index(image_slice, string, 1) * abs(step_string)
                     if reverse:
                         return abs(negative_index(string, pixel_index) + 1), abs(negative_index(image, string_index + 1))
                     return pixel_index, string_index

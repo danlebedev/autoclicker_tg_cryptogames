f26d05c46b3539352d9658fabfec830de8ce2677
danlebedev <lebedev.py@gmail.com>
2024-06-14 12:28:22 +0300

    move click_generator() to tools.py

--- games/games.py
+++ games/games.py
@@ -1,14 +1,5 @@
 from time import sleep
-from random import randint
-
-
-def click_generator(x, y, count=1, x_rand=0, y_rand=0):
-    commands = []
-    for _ in range(count):
-        new_x = randint(-x_rand, x_rand) + x
-        new_y = randint(-y_rand, y_rand) + y
-        commands.append(f"input tap {new_x} {new_y}")
-    return ' && '.join(commands)
+from games.tools import click_generator
 
 
 class HarvestMoon():

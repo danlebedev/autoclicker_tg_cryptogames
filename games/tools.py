75a49e0f0a92c7705e9c8a927461e4cd4b753aa2
danlebedev <lebedev.py@gmail.com>
2024-06-16 11:14:10 +0300

    add load_templates()

--- games/tools.py
+++ games/tools.py
@@ -1,4 +1,8 @@
 from random import randint
+from os import scandir
+from os.path import join, splitext
+from settings import GAMES_DIR
+from PIL import Image
 
 
 def click_generator(x, y, count=1, x_rand=0, y_rand=0):
@@ -8,3 +12,12 @@ def click_generator(x, y, count=1, x_rand=0, y_rand=0):
         new_y = randint(-y_rand, y_rand) + y
         commands.append(f"input tap {new_x} {new_y}")
     return ' && '.join(commands)
+
+
+def load_templates(classname):
+    templates = {}
+    path = join(GAMES_DIR, classname, 'templates')
+    for template in scandir(path):
+        template_name = splitext(template.name)[0]
+        templates[template_name] = Image.open(template.path)
+    return templates

b900fbb112ed7b8a855e1fd79aa04bc913d30fe8
danlebedev <lebedev.py@gmail.com>
2024-06-20 20:23:47 +0300

    rename blum_minigame_script.py and delete unnecessary code

---
+++ blum_script_manual.py
@@ -0,0 +1,61 @@
+from keyboard import is_pressed
+from computer_vision.cv import search_pixel_with_getpixel
+from pyautogui import screenshot
+from pygetwindow import getActiveWindow
+from pynput.mouse import Controller, Button
+
+
+def listen_keyboard():
+    if is_pressed('s'):
+        return True
+    if is_pressed('q'):
+        return False
+
+
+def main():
+    mouse = Controller()
+    drop_min = (153, 201, 0)
+    drop_max = (213, 255, 50)
+
+    last_click = None
+    state = False
+    while True:
+        key = listen_keyboard()
+        if key is not None:
+            if state != key:
+                state = key
+                if state is True:
+                    print('Start')
+                elif state is False:
+                    print('Stop')
+
+        if state is True:
+            window = getActiveWindow()
+            region = (window.left, window.top, window.width, window.height)
+            image = screenshot(region=region)
+            drop_region_coordinates = search_pixel_with_getpixel(
+                image=image,
+                color_min=drop_min,
+                color_max=drop_max,
+                step_string=13,
+                step_pixel=13,
+                reverse=True
+            )
+
+            if drop_region_coordinates:
+                drop_coordinates = (
+                    window.left + drop_region_coordinates[0],
+                    window.top + drop_region_coordinates[1],
+                )
+
+                if drop_coordinates != last_click:
+                    mouse.position = drop_coordinates
+                    mouse.click(Button.left)
+                    last_click = drop_coordinates
+
+
+if __name__ == '__main__':
+    print("""To start press: s
+To stop press: q""")
+    while True:
+        main()
